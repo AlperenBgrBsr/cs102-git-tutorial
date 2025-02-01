e4168cda91ca1f9c6d1aba8f3df509ecc11ab2d9
Alperen Buğra Başar <bugra.basar@ug.bilkent.edu.tr>
2025-02-01 20:02:10 +0300

    Fixed problems within the cipher.java file

--- Cipher.java
+++ Cipher.java
@@ -15,9 +15,8 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+           outputString += inputString.replaceChar(inputString.charAt(v),false);
         }
-
         return outputString;
     }
 
@@ -25,8 +24,10 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        replaceChar('a',true);
+          for (int v=0; i<inputString.length(); v++)
+          {
+             outputString += inputString.replaceChar(inputString.charAt(v),true);
+          }
         
         return outputString;
     }
@@ -42,7 +43,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
